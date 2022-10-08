cb52b3e7b24bea53b30c13b3edc9093462191ad8
mamertmg <marquezgarcia.a@gmail.com>
2022-10-08 16:25:58 +0200

    full code

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,32 @@
-import xw from 'xwind'
-import ButtonReact from '../components/ButtonReact'
-import ButtonStyled from '../components/ButtonStyled'
+import { FeaturedPosts } from '../sections/index';
+import { PostCard, Categories, PostWidget } from '../components';
+import { getPosts } from '../services';
 
-const Index = () => (
-  <div css={xw`grid justify-center items-center h-screen space-y-20`}>
-    <div css={xw`space-y-20`}>
-      <ButtonReact>@emotion/react</ButtonReact>
-      <ButtonStyled>@emotion/styled</ButtonStyled>
+export default function Home({ posts }) {
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <FeaturedPosts />
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post, index) => (
+            <PostCard key={index} post={post.node} />
+          ))}
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-)
+  );
+}
 
-export default Index
+// Fetch data at build time
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+  return {
+    props: { posts },
+  };
+}
